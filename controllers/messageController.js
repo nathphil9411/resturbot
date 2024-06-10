b4c6d1686c43b8665edf439809b469a6e7b7280b
Nathaniel Ezeanaka <ezeanakanath@gmail.com>
2024-06-10 20:53:42 +0100

    add message and order controllers

---
+++ controllers/messageController.js
@@ -0,0 +1,44 @@
+const orderController = require("./orderController");
+
+const sendInitialOptions = (socket) => {
+  const options =
+    "Welcome to naija resturbot, Please select an option:\n1: Place an order\n99: Checkout order\n98: See order history\n97: See current order\n0: Cancel order";
+  socket.emit("message", { text: options });
+};
+
+const handleUserInput = (socket, session, msg) => {
+  switch (msg) {
+    case "1":
+      session.userState === "ordering"
+        ? orderController.handleOrderSelection(socket, session, msg)
+        : orderController.sendMenu(socket, session);
+      session.userState = "ordering";
+      break;
+    case "99":
+      orderController.checkoutOrder(socket, session);
+      session.userState = "mainMenu";
+      break;
+    case "98":
+      orderController.showOrderHistory(socket, session);
+      break;
+    case "97":
+      orderController.showCurrentOrder(socket, session);
+      break;
+    case "0":
+      orderController.cancelOrder(socket, session);
+      session.userState = "mainMenu";
+      break;
+    default:
+      if (session.userState === "ordering") {
+        orderController.handleOrderSelection(socket, session, msg);
+      } else {
+        socket.emit("message", { text: "Invalid option. Please try again." });
+      }
+      break;
+  }
+};
+
+module.exports = {
+  sendInitialOptions,
+  handleUserInput
+};
---
+++ controllers/ordercontroller.js
@@ -0,0 +1,108 @@
+const items = [
+  "Nkwobi",
+  "Amala and Ewedu",
+  "Tuwo Shinkafa",
+  "Owo Soup",
+  "Editan Soup",
+  "Afang Soup",
+  "Banga Soup",
+  "Ugbẹkẹrẹ Soup",
+  "Ukodo (Yam Pepper Soup)",
+  "Ebeji",
+  "Pounded Yam and Gbari Soup",
+  "Fura da Nono",
+  "Okoho Soup",
+  "Achicha",
+  "Yam Porridge",
+  "Waina (Rice Pancakes)",
+  "Pounded Yam and Groundnut Soup",
+  "Apapa Soup",
+  "Gbegiri and Tuwo",
+  "Owghwo Soup"
+];
+
+const sendMenu = (socket, session) => {
+  let menu = "Please select a meal by number: \n";
+  items.forEach((item, index) => {
+    menu += `${index + 1}: ${item}  \n`;
+  });
+  //session.userState = "selectingItem";
+  socket.emit("message", { text: menu });
+};
+
+const handleOrderSelection = (socket, session, msg) => {
+  if (session.currentOrder.length === 1) {
+    socket.emit("message", {
+      text: `please you have an unfinished ${session.currentOrder[0]} order, 99 to checkout, 0 to cancel`
+    });
+    return;
+  }
+  if (session.userState === "ordering") {
+    const index = parseInt(msg) - 1;
+    if (index >= 0 && index < items.length) {
+      session.currentOrder.push(items[index]);
+      socket.emit("message", {
+        text: `${items[index]} added to your order. Press: 99 to Checkout order \n0: Cancel order`
+      });
+    } else {
+      socket.emit("message", { text: "Invalid selection. Please try again." });
+    }
+  } else {
+    socket.emit("message", { text: "Please select a meal first." });
+  }
+};
+
+const checkoutOrder = (socket, session) => {
+  if (session.currentOrder.length > 0) {
+    session.orderHistory.push([...session.currentOrder]);
+    session.currentOrder = [];
+    session.state = "mainMenu";
+    socket.emit("message", {
+      text: "Order placed successfully! Select 1 to place a new order."
+    });
+  } else {
+    socket.emit("message", {
+      text: "No order to place. Select 1 to place a new order."
+    });
+  }
+};
+
+const showOrderHistory = (socket, session) => {
+  if (session.orderHistory.length > 0) {
+    const history = session.orderHistory
+      .map((order, i) => `Order ${i + 1}: ${order.join(", ")}`)
+      .join("\n");
+    socket.emit("message", { text: `Order History:\n${history}` });
+  } else {
+    socket.emit("message", { text: "No order history." });
+  }
+};
+
+const showCurrentOrder = (socket, session) => {
+  if (session.currentOrder.length > 0) {
+    socket.emit("message", {
+      text: `Current Order: ${session.currentOrder.join(", ")}`
+    });
+  } else {
+    socket.emit("message", { text: "No current order." });
+  }
+};
+
+const cancelOrder = (socket, session) => {
+  if (session.currentOrder.length > 0) {
+    session.currentOrder = [];
+    session.state = "mainMenu";
+    socket.emit("message", { text: "Order cancelled." });
+  } else {
+    socket.emit("message", { text: "No order to cancel." });
+  }
+};
+
+module.exports = {
+  sendMenu,
+  handleOrderSelection,
+  checkoutOrder,
+  showOrderHistory,
+  showCurrentOrder,
+  cancelOrder
+};
